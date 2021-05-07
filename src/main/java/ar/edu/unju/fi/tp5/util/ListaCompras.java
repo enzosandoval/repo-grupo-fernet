e6e96e7458aa617aded2f6ea2871a8a02fbf4e77
Gabriel Sardina <gabinex15@gmail.com>
2021-05-06 23:34:14 -0300

    Clase ListaCliente - ArrayList creado

---
+++ src/main/java/ar/edu/unju/fi/tp5/util/ListaCompras.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.tp5.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.tp5.model.Compra;
+
+public class ListaCompras {
+
+
+	// ================================================ -¬
+	// == ArrayList para guardar los objetos compras ==  |
+	// ================================================  V
+	
+	public static List<Compra> listaCompras = new ArrayList<Compra>();
+}
