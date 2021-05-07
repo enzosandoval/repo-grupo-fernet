3fcdc705db044c31e3edb7de508f9dbaac1ba5f7
GabrielPc <GabrielPc@DESKTOP-QRRK269>
2021-05-07 02:37:47 -0300

    Agrego la interface y la clase implementadora

---
+++ src/main/java/ar/edu/unju/fi/tp5/service/ICompraService.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.tp5.service;
+
+import java.util.List;
+
+import ar.edu.unju.fi.tp5.model.Compra;
+
+
+/**
+ * @author Team Fernet
+ *
+ */
+public interface ICompraService {
+
+	public void guardarCompra (Compra compra);
+	
+	public Compra obtenerUltimaCompra();
+	
+	public List<Compra> obtenerLista();
+}
